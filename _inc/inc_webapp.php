d2fa9860616c21ebf4aed6dcfb020c4383e3efcb
olivier VT <olivier.vantriel@gmail.com>
2015-09-27 19:21:06 +0200

    app information update

--- _inc/inc_webapp.php
+++ _inc/inc_webapp.php
@@ -3,7 +3,7 @@
 <meta name="apple-mobile-web-app-title" content=""/>
 <meta name="mobile-web-app-capable" content="yes"/>
 
-<link rel="apple-touch-icon" href=""/>        
-<link rel="apple-touch-icon" sizes="76x76" href=""/>
-<link rel="apple-touch-icon" sizes="120x120" href=""/>
-<link rel="apple-touch-icon" sizes="152x152" href=""/>
+<link rel="apple-touch-icon" href="../img/ci.jpg"/>        
+<link rel="apple-touch-icon" sizes="76x76" href="../img/ci.jpg"/>
+<link rel="apple-touch-icon" sizes="120x120" href="../img/ci.jpg"/>
+<link rel="apple-touch-icon" sizes="152x152" href="../img/ci.jpg"/>
